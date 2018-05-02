cacf20b3d65a97982aa997e15622cee32edf877d
martin Hwang <dhhwang89@gmail.com>
2018-05-03 00:59:23 +0900

    1. bug fix in example.py with VOC Convert to YOLO

--- example.py
+++ example.py
@@ -29,17 +29,18 @@ def main(config):
         flag, data = voc.parse(config["label"])
 
         if flag == True:
-            flag, data = voc.generate(data)
 
+            flag, data = yolo.generate(data)
             if flag == True:
                 flag, data = yolo.save(data, config["output_path"], config["img_path"] ,
-                                        config["img_type"], config["manipast_path"])
+                                       config["img_type"], config["manipast_path"])
 
                 if flag == False:
                     print("Saving Result : {}, msg : {}".format(flag, data))
 
             else:
                 print("YOLO Generating Result : {}, msg : {}".format(flag, data))
+
 
         else:
             print("VOC Parsing Result : {}, msg : {}".format(flag, data))
